fe0fb3d126426ba6da8a12e44e974528739bf584
citardel <citardel@gmail.com>
2024-02-17 09:14:12 +0100

    dfx configuration

---
+++ src/pantaker_frontend/src/index.jsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+import ReactDOM from "react-dom"; // Import ReactDOM
+import "../assets/main.css";
+import App from './app';
+
+ReactDOM.render(<App />, document.getElementById('root'));
